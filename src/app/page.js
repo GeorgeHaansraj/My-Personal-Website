017eff1c827b49acebb576d8f033b58e44022200
unknown <georgehaansraj@gmial.com>
2025-06-16 23:35:29 +0700

    edit page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="text-center mt-20">
       <h1 className="text-4xl font-bold">Hi, I&apos;m George Haansraj</h1>
       <p className="text-lg text-gray-600 mt-4">Tech | Life | Boxing | Refleksi</p>
+      <p>This is my personal website. Stay tuned!</p>
     </main>
   );
 }
