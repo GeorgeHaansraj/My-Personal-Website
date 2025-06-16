44a85c8994a01724605f87d215f7203557ef4a2f
unknown <georgehaansraj@gmial.com>
2025-06-16 23:12:43 +0700

    Fix unescaped apostrophes for ESLint

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="text-center mt-20">
-      <h1 className="text-4xl font-bold">Hi, I'm George Haansraj</h1>
+      <h1 className="text-4xl font-bold">Hi, I&apos;m George Haansraj</h1>
       <p className="text-lg text-gray-600 mt-4">Tech | Life | Boxing | Refleksi</p>
     </main>
   );
